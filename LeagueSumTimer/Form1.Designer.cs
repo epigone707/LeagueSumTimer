75774d5a456795cc54c5ef46f5b8f15de2803e17
Yanfu <yanfuguo@umich.edu>
2022-08-22 11:50:52 -0400

    Initial commit

---
+++ LeagueSumTimer/Form1.Designer.cs
@@ -0,0 +1,231 @@
+﻿namespace LeagueSumTimer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnClipboard = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnTopFlash = new System.Windows.Forms.Button();
+            this.btnJugFlash = new System.Windows.Forms.Button();
+            this.btnMidFlash = new System.Windows.Forms.Button();
+            this.btnBotFlash = new System.Windows.Forms.Button();
+            this.btnSupFlash = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnClipboard
+            // 
+            this.btnClipboard.Location = new System.Drawing.Point(45, 434);
+            this.btnClipboard.Name = "btnClipboard";
+            this.btnClipboard.Size = new System.Drawing.Size(75, 23);
+            this.btnClipboard.TabIndex = 4;
+            this.btnClipboard.Text = "button1";
+            this.btnClipboard.UseVisualStyleBackColor = true;
+            this.btnClipboard.Click += new System.EventHandler(this.btnClipboard_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::LeagueSumTimer.Properties.Resources.TOP;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(72, 65);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnTopFlash
+            // 
+            this.btnTopFlash.BackgroundImage = global::LeagueSumTimer.Properties.Resources.Flash;
+            this.btnTopFlash.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnTopFlash.FlatAppearance.BorderSize = 0;
+            this.btnTopFlash.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnTopFlash.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTopFlash.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnTopFlash.Location = new System.Drawing.Point(101, 12);
+            this.btnTopFlash.Margin = new System.Windows.Forms.Padding(0);
+            this.btnTopFlash.Name = "btnTopFlash";
+            this.btnTopFlash.Size = new System.Drawing.Size(64, 64);
+            this.btnTopFlash.TabIndex = 1;
+            this.btnTopFlash.UseVisualStyleBackColor = true;
+            this.btnTopFlash.Click += new System.EventHandler(this.btnTopFlash_Click);
+            // 
+            // btnJugFlash
+            // 
+            this.btnJugFlash.BackgroundImage = global::LeagueSumTimer.Properties.Resources.Flash;
+            this.btnJugFlash.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnJugFlash.FlatAppearance.BorderSize = 0;
+            this.btnJugFlash.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnJugFlash.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnJugFlash.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnJugFlash.Location = new System.Drawing.Point(101, 92);
+            this.btnJugFlash.Margin = new System.Windows.Forms.Padding(0);
+            this.btnJugFlash.Name = "btnJugFlash";
+            this.btnJugFlash.Size = new System.Drawing.Size(64, 64);
+            this.btnJugFlash.TabIndex = 5;
+            this.btnJugFlash.UseVisualStyleBackColor = true;
+            this.btnJugFlash.Click += new System.EventHandler(this.btnJugFlash_Click);
+            // 
+            // btnMidFlash
+            // 
+            this.btnMidFlash.BackgroundImage = global::LeagueSumTimer.Properties.Resources.Flash;
+            this.btnMidFlash.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnMidFlash.FlatAppearance.BorderSize = 0;
+            this.btnMidFlash.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnMidFlash.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMidFlash.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnMidFlash.Location = new System.Drawing.Point(101, 171);
+            this.btnMidFlash.Margin = new System.Windows.Forms.Padding(0);
+            this.btnMidFlash.Name = "btnMidFlash";
+            this.btnMidFlash.Size = new System.Drawing.Size(64, 64);
+            this.btnMidFlash.TabIndex = 6;
+            this.btnMidFlash.UseVisualStyleBackColor = true;
+            // 
+            // btnBotFlash
+            // 
+            this.btnBotFlash.BackgroundImage = global::LeagueSumTimer.Properties.Resources.Flash;
+            this.btnBotFlash.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnBotFlash.FlatAppearance.BorderSize = 0;
+            this.btnBotFlash.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnBotFlash.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBotFlash.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnBotFlash.Location = new System.Drawing.Point(101, 251);
+            this.btnBotFlash.Margin = new System.Windows.Forms.Padding(0);
+            this.btnBotFlash.Name = "btnBotFlash";
+            this.btnBotFlash.Size = new System.Drawing.Size(64, 64);
+            this.btnBotFlash.TabIndex = 7;
+            this.btnBotFlash.UseVisualStyleBackColor = true;
+            // 
+            // btnSupFlash
+            // 
+            this.btnSupFlash.BackgroundImage = global::LeagueSumTimer.Properties.Resources.Flash;
+            this.btnSupFlash.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnSupFlash.FlatAppearance.BorderSize = 0;
+            this.btnSupFlash.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSupFlash.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSupFlash.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnSupFlash.Location = new System.Drawing.Point(101, 329);
+            this.btnSupFlash.Margin = new System.Windows.Forms.Padding(0);
+            this.btnSupFlash.Name = "btnSupFlash";
+            this.btnSupFlash.Size = new System.Drawing.Size(64, 64);
+            this.btnSupFlash.TabIndex = 8;
+            this.btnSupFlash.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::LeagueSumTimer.Properties.Resources.JUNGLE;
+            this.pictureBox2.Location = new System.Drawing.Point(12, 92);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(72, 65);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 9;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::LeagueSumTimer.Properties.Resources.MIDDLE;
+            this.pictureBox3.Location = new System.Drawing.Point(12, 170);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(72, 65);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox3.TabIndex = 10;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = global::LeagueSumTimer.Properties.Resources.ADC;
+            this.pictureBox4.Location = new System.Drawing.Point(12, 251);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(72, 65);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox4.TabIndex = 11;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Image = global::LeagueSumTimer.Properties.Resources.SUPPORT;
+            this.pictureBox5.Location = new System.Drawing.Point(12, 329);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(72, 65);
+            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox5.TabIndex = 12;
+            this.pictureBox5.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(51)))), ((int)(((byte)(73)))));
+            this.ClientSize = new System.Drawing.Size(186, 463);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.btnSupFlash);
+            this.Controls.Add(this.btnBotFlash);
+            this.Controls.Add(this.btnMidFlash);
+            this.Controls.Add(this.btnJugFlash);
+            this.Controls.Add(this.btnClipboard);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnTopFlash);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Padding = new System.Windows.Forms.Padding(3);
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnTopFlash;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnClipboard;
+        private System.Windows.Forms.Button btnJugFlash;
+        private System.Windows.Forms.Button btnMidFlash;
+        private System.Windows.Forms.Button btnBotFlash;
+        private System.Windows.Forms.Button btnSupFlash;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+    }
+}
+
---
+++ LeagueSumTimer/Form1.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Windows.Forms;
+
+
+namespace LeagueSumTimer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            topTimer.Tick += new EventHandler(topTimerEventProcessor);
+            jugTimer.Tick += new EventHandler(jugTimerEventProcessor);
+        }
+
+        private int[] FlashTotalCooldown = { 300, 300, 300, 300, 300 };
+
+        static System.Windows.Forms.Timer topTimer = new System.Windows.Forms.Timer();
+        static int [] Counter = { 0, 0, 0, 0, 0 };
+        static bool [] ExitFlag = { false, false, false, false, false };
+
+        static System.Windows.Forms.Timer jugTimer = new System.Windows.Forms.Timer();
+
+        static System.Windows.Forms.Timer midTimer = new System.Windows.Forms.Timer();
+
+        static System.Windows.Forms.Timer botTimer = new System.Windows.Forms.Timer();
+
+        static System.Windows.Forms.Timer supTimer = new System.Windows.Forms.Timer();
+
+
+        private void btnTopFlash_Click(object sender, EventArgs e)
+        {
+            topTimer.Stop();
+            Console.WriteLine("btnTopFlash_Click...");
+            btnTopFlash.Text = "300";
+            int role = 0;
+            Counter[role] = 300;
+            btnTopFlash.BackgroundImage = Properties.Resources.FlashNotReady;
+
+            
+            
+            // Sets the timer interval to 5 seconds.
+            topTimer.Interval = 1000;
+            topTimer.Start();
+
+            //// Runs the timer, and raises the event.
+            //while (ExitFlag[role] == false)
+            //{
+            //    // Processes all the events in the queue.
+
+            //    Application.DoEvents();
+            //}
+
+
+
+        }
+        private void topTimerEventProcessor(Object myObject,
+                                            EventArgs myEventArgs)
+        {
+            int role = 0;
+            topTimer.Stop();
+
+            if (Counter[role] > 0)
+            {
+                // Restarts the timer and increments the counter.
+                Counter[role] -= 1;
+                topTimer.Enabled = true;
+                btnTopFlash.Text = Counter[role].ToString();
+
+            }
+            else
+            {
+                // Stops the timer.
+                ExitFlag[role] = true;
+                btnTopFlash.Text = "";
+                btnTopFlash.BackgroundImage = Properties.Resources.Flash;
+            }
+        }
+
+
+
+        private void btnJugFlash_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine("btnJugFlash_Click...");
+            btnJugFlash.Text = "300";
+            int role = 1;
+            Counter[role] = 300;
+            btnJugFlash.BackgroundImage = Properties.Resources.FlashNotReady;
+
+            
+            
+            // Sets the timer interval to 5 seconds.
+            jugTimer.Interval = 1000;
+            jugTimer.Start();
+            // Runs the timer, and raises the event.
+            //while (ExitFlag[role] == false)
+            //{
+            //    // Processes all the events in the queue.
+            //    Application.DoEvents();
+            //}
+
+            
+            
+
+        }
+        private void jugTimerEventProcessor(Object myObject,
+                                            EventArgs myEventArgs)
+        {
+            int role = 1;
+            jugTimer.Stop();
+
+            if (Counter[role] > 0)
+            {
+                // Restarts the timer and increments the counter.
+                Counter[role] -= 1;
+                jugTimer.Enabled = true;
+                btnJugFlash.Text = Counter[role].ToString();
+
+            }
+            else
+            {
+                // Stops the timer.
+                ExitFlag[role] = true;
+                btnJugFlash.Text = "";
+                btnJugFlash.BackgroundImage = Properties.Resources.Flash;
+            }
+        }
+
+        private void btnClipboard_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine("btnClipboard_Click...");
+            Clipboard.SetText("Hello, clipboard");
+        }
+
+    }
+}
